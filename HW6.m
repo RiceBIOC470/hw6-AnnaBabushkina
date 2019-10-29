dee56f4fc4ddc8746301c8b8121a410a285148e4
warmflasha <warmflasha@gmail.com>
2019-10-29 12:52:21 -0500

    Added hw6

---
+++ HW6.m
@@ -0,0 +1,25 @@
+%% Problem 1. Starting with Ilastik
+
+% Part 1. Use Ilastik to perform a segmentation of the image stemcells.tif
+% in this folder. Be conservative about what you call background - i.e.
+% don't mark something as background unless you are sure it is background.
+% Output your mask into your repository. What is the main problem with your segmentation?  
+
+% Part 2. Read you segmentation mask from Part 1 into MATLAB and use
+% whatever methods you can to try to improve it. 
+
+% Part 3. Redo part 1 but now be more aggresive in defining the background.
+% Try your best to use ilastik to separate cells that are touching. Output
+% the resulting mask into the repository. What is the problem now?
+
+% Part 4. Read your mask from Part 3 into MATLAB and try to improve
+% it as best you can.
+
+%% Problem 2. Segmentation problems.
+
+% The folder segmentationData has 4 very different images. Use
+% whatever tools you like to try to segement the objects the best you can. Put your code and
+% output masks in the repository. If you use Ilastik as an intermediate
+% step put the output from ilastik in your repository as well as an .h5
+% file. Put code here that will allow for viewing of each image together
+% with your final segmentation. 
